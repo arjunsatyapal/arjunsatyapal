74c9209da694b9c7c066d3bc34e4213e60a1613c
Arjun Satyapal <arjunsatyapal@gmail.com>
2011-04-25 21:53:47 +0000

    Deleting whole practice

--- trunk/practice/gwt/essential_gwt/gwt1/src/com/arjunsatyapal/practice/gwt1/client/HelloBrowserStdImpl.java
+++
@@ -1,9 +0,0 @@
-package com.arjunsatyapal.practice.gwt1.client;
-
-import com.google.gwt.user.client.Window;
-
-public class HelloBrowserStdImpl {
-  public void sayHello() {
-    Window.alert("You don't have IE.");
-  }
-}
--- trunk/practice/gwt/essential_gwt/gwt4-mine/src/com/arjunsatyapal/practice/gwt1/client/Display.java
+++
@@ -1,7 +0,0 @@
-package com.arjunsatyapal.practice.gwt1.client;
-
-import com.google.gwt.user.client.ui.Widget;
-
-public interface Display {
-  public Widget asWidget();
-}
