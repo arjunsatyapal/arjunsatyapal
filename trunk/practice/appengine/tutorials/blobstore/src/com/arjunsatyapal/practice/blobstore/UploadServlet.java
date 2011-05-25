e77242df73e06f78012bdfcd47d5bd5227e91129
Arjun Satyapal <arjunsatyapal@gmail.com>
2011-05-25 19:56:19 +0000

    adding tutorial for blobstore on appengine.

---
+++ trunk/practice/appengine/tutorials/blobstore/src/com/arjunsatyapal/practice/blobstore/UploadServlet.java
@@ -0,0 +1,30 @@
+package com.arjunsatyapal.practice.blobstore;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UploadServlet extends HttpServlet {
+    private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+        throws IOException {
+
+        Map<String, BlobKey> blobs = blobstoreService.getUploadedBlobs(req);
+        BlobKey blobKey = blobs.get("myFile");
+
+        if (blobKey == null) {
+            res.sendRedirect("/");
+        } else {
+            res.sendRedirect("/serveServlet?blob-key=" + blobKey.getKeyString());
+        }
+    }
+}
