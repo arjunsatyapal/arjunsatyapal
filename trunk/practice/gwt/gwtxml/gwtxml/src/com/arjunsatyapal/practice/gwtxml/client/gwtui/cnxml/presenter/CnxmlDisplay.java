b0834c1bfcb8c9c3800e1c89ab22cd7f9793e17c
Arjun Satyapal <arjunsatyapal@gmail.com>
2011-06-06 10:56:49 +0000

    adding scroll bar

--- trunk/practice/gwt/gwtxml/gwtxml/src/com/arjunsatyapal/practice/gwtxml/client/gwtui/cnxml/presenter/CnxmlDisplay.java
+++ trunk/practice/gwt/gwtxml/gwtxml/src/com/arjunsatyapal/practice/gwtxml/client/gwtui/cnxml/presenter/CnxmlDisplay.java
@@ -1,9 +1,9 @@
 package com.arjunsatyapal.practice.gwtxml.client.gwtui.cnxml.presenter;
 
-import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.Panel;
 
 import com.arjunsatyapal.practice.gwtxml.client.gwtui.mvpinterfaces.Display;
 
 public interface CnxmlDisplay extends Display {
-    LayoutPanel getLayoutPanel();
+    Panel getLayoutPanel();
 }
--- trunk/practice/gwt/gwtxml/gwtxml/src/com/arjunsatyapal/practice/gwtxml/client/gwtui/cnxml/presenter/CnxmlView.java
+++ trunk/practice/gwt/gwtxml/gwtxml/src/com/arjunsatyapal/practice/gwtxml/client/gwtui/cnxml/presenter/CnxmlView.java
@@ -3,14 +3,15 @@ package com.arjunsatyapal.practice.gwtxml.client.gwtui.cnxml.presenter;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import com.arjunsatyapal.practice.gwtxml.client.gwtui.mvpinterfaces.View;
 
 public class CnxmlView extends View implements CnxmlDisplay {
   @UiField
-  LayoutPanel layoutPanel;
+  ScrollPanel layoutPanel;
 
   private static CnxmlWidgetUiBinder uiBinder = GWT
       .create(CnxmlWidgetUiBinder.class);
@@ -23,7 +24,7 @@ public class CnxmlView extends View implements CnxmlDisplay {
   }
 
   @Override
-  public LayoutPanel getLayoutPanel() {
+  public Panel getLayoutPanel() {
     return layoutPanel;
   }
 
