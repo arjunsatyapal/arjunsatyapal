240f6711e88c3525ba5ffb8fc7bddf6f946de16e
Arjun Satyapal <arjunsatyapal@gmail.com>
2011-07-20 00:45:00 +0000

    committing libraries.

--- trunk/practice/rest/jaxrs/restapi/src/com/arjunsatyapal/restapi/RestapiServlet.java
+++ trunk/practice/rest/jaxrs/restapi/src/com/arjunsatyapal/restapi/RestapiServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class RestapiServlet extends HttpServlet {
+    @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
         resp.setContentType("text/plain");
