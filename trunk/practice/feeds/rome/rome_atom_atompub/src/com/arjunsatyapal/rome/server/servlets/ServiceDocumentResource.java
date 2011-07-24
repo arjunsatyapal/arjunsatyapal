12e095f3825613880f6c93b2a9166aee9b2b71e0
Arjun Satyapal <arjuns@google.com>
2011-07-24 15:48:50 -0700

    fixing warnings

--- trunk/practice/feeds/rome/rome_atom_atompub/src/com/arjunsatyapal/rome/server/servlets/ServiceDocumentResource.java
+++ trunk/practice/feeds/rome/rome_atom_atompub/src/com/arjunsatyapal/rome/server/servlets/ServiceDocumentResource.java
@@ -31,7 +31,6 @@ import org.jdom.output.XMLOutputter;
 import java.io.IOException;
 import java.io.Writer;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
