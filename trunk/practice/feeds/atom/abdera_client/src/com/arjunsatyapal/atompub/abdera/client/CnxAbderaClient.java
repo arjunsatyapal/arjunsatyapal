08c38fa3495c2b06455bedfcadf4381c6cb469ed
Arjun Satyapal <arjuns@google.com>
2011-07-24 21:02:44 -0700

    refactored abdera client

---
+++ trunk/practice/feeds/atom/abdera_client/src/com/arjunsatyapal/atompub/abdera/client/CnxAbderaClient.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.arjunsatyapal.atompub.abdera.client;
+
+import static com.arjunsatyapal.atompub.abdera.client.CnxClientConstants.CNX_WORKSPACE;
+import static com.arjunsatyapal.atompub.abdera.client.CnxClientConstants.COLLECTION_CNX_RESOURCE;
+
+import org.apache.abdera.Abdera;
+import org.apache.abdera.factory.Factory;
+import org.apache.abdera.model.Collection;
+import org.apache.abdera.model.Document;
+import org.apache.abdera.model.Service;
+import org.apache.abdera.model.Workspace;
+import org.apache.abdera.protocol.client.AbderaClient;
+
+/**
+ *
+ * @author Arjun Satyapal
+ */
+public class CnxAbderaClient {
+    
+    public static void main(String[] args) {
+        
+        CnxClientConstants clientConstants = new CnxClientConstants(!false);
+        
+        Abdera abdera = new Abdera();
+        AbderaClient abderaClient = new AbderaClient(abdera);
+        Factory factory = abdera.getFactory();
+
+        // Fetch ServiceDocuemnt
+         Document<Service> serviceDocument = abderaClient.get(clientConstants.getServiceDocumentUrl()).getDocument();
+         Service service = serviceDocument.getRoot();
+         
+         // Get Workspace.
+         Workspace workSpace = service.getWorkspace(CNX_WORKSPACE);
+         
+         // Get Resource Collection.
+         Collection collection = workSpace.getCollection(COLLECTION_CNX_RESOURCE);
+         
+         // Create a Resource.
+         
+         System.out.println("*****************done.");
+    }
+}
---
+++ trunk/practice/feeds/atom/abdera_client/src/com/arjunsatyapal/atompub/abdera/client/CnxClientConstants.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.arjunsatyapal.atompub.abdera.client;
+
+/**
+ * 
+ * @author Arjun Satyapal
+ */
+public class CnxClientConstants {
+    private String host;
+    private String serviceDocumentUrl;
+
+    // TODO(arjuns) : Share constants with server.
+    public static final String CNX_WORKSPACE = "Connexions Workspace";
+    public static final String COLLECTION_CNX_RESOURCE = "AtomPub Collection for CNX Resources.";
+    public static final String COLLECTION_CNX_MODULE = "AtomPub Collection for CNX Modules.";
+    public static final String COLLECTION_CNX_COLLECTION = "AtomPub Collection for Cnx Collections.";
+    
+    public CnxClientConstants(boolean isLocal) {
+        if (isLocal) {
+            this.host = "http://localhost:8888";
+        } else {
+            this.host = "http://arjuns-test.appspot.com";
+        }
+        
+        this.serviceDocumentUrl = this.host + "/atompub/service_document";
+    }
+
+    public String getServiceDocumentUrl() {
+        return serviceDocumentUrl;
+    }
+}
