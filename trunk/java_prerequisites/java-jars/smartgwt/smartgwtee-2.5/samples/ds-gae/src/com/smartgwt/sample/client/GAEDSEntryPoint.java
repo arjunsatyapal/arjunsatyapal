f616784e4c11508989ade6b6600e8d10d35e6be0
Arjun Satyapal <arjuns@google.com>
2011-10-12 16:17:19 -0700

    commiting changes done by me.

--- trunk/java_prerequisites/java-jars/smartgwt/smartgwtee-2.5/samples/ds-gae/src/com/smartgwt/sample/client/GAEDSEntryPoint.java
+++ trunk/java_prerequisites/java-jars/smartgwt/smartgwtee-2.5/samples/ds-gae/src/com/smartgwt/sample/client/GAEDSEntryPoint.java
@@ -1,5 +1,15 @@
 package com.smartgwt.sample.client;
 
+import com.smartgwt.client.widgets.Label;
+
+import com.smartgwt.client.widgets.layout.LayoutSpacer;
+
+import com.smartgwt.client.widgets.Img;
+
+import com.smartgwt.client.widgets.layout.HLayout;
+
+import com.smartgwt.client.widgets.form.DynamicForm;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.smartgwt.client.core.KeyIdentifier;
 import com.smartgwt.client.data.DataSource;
@@ -32,19 +42,108 @@ public class GAEDSEntryPoint implements EntryPoint {
     }
 
     /**
-     * The entry point method, called automatically by loading a module
-     * that declares an implementing class as an entry-point
+     * The entry point method, called automatically by loading a module that declares an
+     * implementing class as an entry-point
      */
     public void onModuleLoad() {
-		KeyIdentifier debugKey = new KeyIdentifier();
-		debugKey.setCtrlKey(true);
-		debugKey.setKeyName("D");
+        // handleOriginal();
+        
+        // URL : http://www.smartclient.com/releases/SmartGWT_Quick_Start_Guide.pdf
+        // Page : 18
+//        handleListGrid();
+        
+        // URL : http://www.smartclient.com/releases/SmartGWT_Quick_Start_Guide.pdf
+        // Page 21
+//        handleContactsDataSource();
+        
+     // URL : http://www.smartclient.com/releases/SmartGWT_Quick_Start_Guide.pdf
+        // Page 30
+        handleLayouts();
+        
+    }
 
-		Page.registerKey(debugKey, new KeyCallback() {
-			public void execute(String keyName) {
-				SC.showConsole();
-			}
-		});
+    /**
+     * 
+     */
+    private void handleLayouts() {
+        HLayout hLayout = new HLayout(10);
+        hLayout.setID("myPageHeader");
+        hLayout.setHeight(50);
+        hLayout.setLayoutMargin(10);
+        hLayout.addMember(new Img("lantern_logo.png"));
+        hLayout.addMember(new LayoutSpacer());
+        hLayout.addMember(new Label("My Title"));
+        hLayout.draw();
+    }
+
+    /**
+     * 
+     */
+    private void handleContactsDataSource() {
+        DataSource contactsDS = DataSource.get("contacts");
+        ListGrid grid = new ListGrid();
+        grid.setDataSource(contactsDS);
+        DynamicForm form = new DynamicForm();
+        form.setLeft(300); // avoid overlap
+        form.setDataSource(contactsDS);
+        
+        grid.setWidth100();
+        grid.draw();
+    }
+
+    /**
+     * 
+     */
+    private void handleListGrid() {
+        // TODO Auto-generated method stub
+        ListGrid grid = new ListGrid();
+        ListGridField salutationField = new ListGridField();
+        salutationField.setName("salutation");
+        salutationField.setTitle("Title");
+        ListGridField firstNameField = new ListGridField();
+        firstNameField.setName("firstname");
+        firstNameField.setTitle("First Name");
+        ListGridField lastNameField = new ListGridField();
+        lastNameField.setName("lastname");
+        lastNameField.setTitle("Last Name");
+        grid.setFields(salutationField,
+                firstNameField,
+                lastNameField);
+        grid.setData(getListGridRecords());
+        grid.draw();
+
+    }
+    
+    private ListGridRecord[] getListGridRecords() {
+        ListGridRecord record1 = new ListGridRecord();
+        record1.setAttribute("salutation", "Ms");
+        record1.setAttribute("firstname", "Kathy");
+        record1.setAttribute("lastname", "Whitting");
+        ListGridRecord record2 = new ListGridRecord();
+        record2.setAttribute("salutation", "Mr");
+        record2.setAttribute("firstname", "Chris");
+        record2.setAttribute("lastname", "Glover");
+        ListGridRecord record3 = new ListGridRecord();
+        record3.setAttribute("salutation", "Mrs");
+        record3.setAttribute("firstname", "Gwen");
+        record3.setAttribute("lastname", "Glover");
+        
+        return new ListGridRecord[] {record1, record2, record3};
+    }
+
+    /**
+     * 
+     */
+    private void handleOriginal() {
+        KeyIdentifier debugKey = new KeyIdentifier();
+        debugKey.setCtrlKey(true);
+        debugKey.setKeyName("D");
+
+        Page.registerKey(debugKey, new KeyCallback() {
+            public void execute(String keyName) {
+                SC.showConsole();
+            }
+        });
 
         VStack vStack = new VStack();
         vStack.setLeft(175);
@@ -73,7 +172,7 @@ public class GAEDSEntryPoint implements EntryPoint {
         IButton newCountryButton = new IButton("New country");
         newCountryButton.addClickHandler(new ClickHandler()
         {
-            public void onClick (ClickEvent event)
+            public void onClick(ClickEvent event)
             {
                 countryGrid.startEditingNew();
             }
@@ -98,7 +197,7 @@ public class GAEDSEntryPoint implements EntryPoint {
         IButton newCityButton = new IButton("New city");
         newCityButton.addClickHandler(new ClickHandler()
         {
-            public void onClick (ClickEvent event)
+            public void onClick(ClickEvent event)
             {
                 ListGridRecord record = countryGrid.getSelectedRecord();
                 if (record != null) {
@@ -112,7 +211,7 @@ public class GAEDSEntryPoint implements EntryPoint {
 
         countryGrid.addSelectionChangedHandler(new SelectionChangedHandler()
         {
-            public void onSelectionChanged (SelectionEvent event)
+            public void onSelectionChanged(SelectionEvent event)
             {
                 if (event.getState()) {
                     ListGridRecord record = countryGrid.getSelectedRecord();
@@ -125,22 +224,22 @@ public class GAEDSEntryPoint implements EntryPoint {
 
         cityGrid.addEditorEnterHandler(new EditorEnterHandler()
         {
-            public void onEditorEnter (EditorEnterEvent event)
+            public void onEditorEnter(EditorEnterEvent event)
             {
                 cityGrid.setEditValue(event.getRowNum(), "countryId",
-                                      countryGrid.getSelectedRecord().getAttribute("countryId"));
+                        countryGrid.getSelectedRecord().getAttribute("countryId"));
             }
         });
 
         IButton fillDataButton = new IButton("Add some data");
         fillDataButton.addClickHandler(new ClickHandler() {
-            public void onClick (ClickEvent event)
+            public void onClick(ClickEvent event)
             {
                 RPCRequest req = new RPCRequest();
                 req.setActionURL("/fillData");
                 RPCManager.sendRequest(req, new RPCCallback() {
-                    public void execute (RPCResponse response, Object rawData,
-                                         RPCRequest request)
+                    public void execute(RPCResponse response, Object rawData,
+                            RPCRequest request)
                     {
                         countryGrid.fetchData();
                     }
